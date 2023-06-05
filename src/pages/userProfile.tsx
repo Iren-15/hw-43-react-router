491155280c5e1e71bd2c806a8a15cd20a55aec22
Iren-15 <131612844+Iren-15@users.noreply.github.com>
2023-06-05 23:08:25 +0300

    user api

--- src/pages/userProfile.tsx
+++ src/pages/userProfile.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../providers/my-provider";
 import { useParams } from "react-router-dom";
 
@@ -10,13 +10,33 @@ export const UserProfile = () => {
     return user.id == params.userId;
   });
 
+  const userSelectedLogin = userSelected?.login;
+  //console.log("ulogin:", userSelectedLogin);
+  //console.log("params.userId", params.userId);
+
+  const [userSelectedApi, setUserSelectedApi] = useState();
+  useEffect(() => {
+    const getUsers = async () => {
+      const rez = await fetch(
+        `https://api.github.com/users/${userSelectedLogin}`
+      );
+      const rezReceived = await rez.json();
+      setUserSelectedApi(rezReceived);
+      //console.log("rezReceived:", rezReceived);
+    };
+    getUsers();
+  }, [userSelectedLogin]);
+
   return (
     <div>
       <h2>User`s info:</h2>
-      <p>Login: {userSelected?.login}</p>
+      <p>Login: {userSelectedApi?.login}</p>
+      <img src={userSelectedApi?.avatar_url} width="100px" alt="photo"></img>
+      <p>Url: {userSelectedApi?.url}</p>
+      {/* <p>Login: {userSelected?.login}</p>
       <img src={userSelected?.avatar_url} width="100px" alt="photo"></img>
       <p>Url: {userSelected?.url}</p>
-      <p>Id: {userSelected?.id}</p>
+      <p>Id: {userSelected?.id}</p> */}
     </div>
   );
 };
